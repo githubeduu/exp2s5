c305164fb67791e6cde2bb2fe7cb53635ef37cdc
Eduardo <edo.carvajal@hotmail.com>
2024-04-21 16:07:14 -0400

    integración hateoas

--- src/main/java/com/example/exp2s5/controller/PeliculaController.java
+++ src/main/java/com/example/exp2s5/controller/PeliculaController.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import com.example.exp2s5.model.Pelicula;
 import com.example.exp2s5.service.PeliculaService;
 
-import jakarta.persistence.Entity;
 import java.util.stream.Collectors;
+import java.util.Collections;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +24,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/pelicula")
@@ -34,36 +34,36 @@ public class PeliculaController {
     @Autowired
     private PeliculaService peliculaService;
 
-
     @GetMapping
     public CollectionModel<EntityModel<Pelicula>> getAllPeliculas() {
-       List<Pelicula> peliculas = peliculaService.getAllPelicula();
-       log.info("GET /peliculas");
-       log.info("Retornando todas las peliculas");
-       List<EntityModel<Pelicula>> peliculasResources = peliculas.stream()
-            .map( pelicula -> EntityModel.of(pelicula,
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(pelicula.getId())).withSelfRel()
-            ))
-            .collect(Collectors.toList());
+        List<Pelicula> peliculas = peliculaService.getAllPelicula();
+        log.info("GET /peliculas");
+        log.info("Retornando todas las peliculas");
+        List<EntityModel<Pelicula>> peliculasResources = peliculas.stream()
+                .map(pelicula -> EntityModel.of(pelicula,
+                        WebMvcLinkBuilder
+                                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(pelicula.getId()))
+                                .withSelfRel()))
+                .collect(Collectors.toList());
 
-        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas());
-        CollectionModel<EntityModel<Pelicula>> resources = CollectionModel.of(peliculasResources, linkTo.withRel("Peliculas"));
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder
+                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas());
+        CollectionModel<EntityModel<Pelicula>> resources = CollectionModel.of(peliculasResources,
+                linkTo.withRel("Peliculas"));
 
         return resources;
-    } 
-
-
-
-    // @GetMapping
-    // public List<Pelicula> getAllPeliculas() {
-    //     return peliculaService.getAllPelicula();
-    // }
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getPeliculaById(@PathVariable Long id) {
         Optional<Pelicula> pelicula = peliculaService.getPeliculaById(id);
         if (pelicula.isPresent()) {
-            return ResponseEntity.ok(pelicula.get());
+            EntityModel<Pelicula> resource = EntityModel.of(pelicula.get(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(id))
+                            .withSelfRel(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas())
+                            .withRel("all-peliculas"));
+            return ResponseEntity.ok(resource);
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pelicula no encontrada");
         }
@@ -73,20 +73,34 @@ public class PeliculaController {
     public ResponseEntity<?> createPelicula(@Validated @RequestBody Pelicula pelicula) {
         try {
             Pelicula nuevaPelicula = peliculaService.createPelicula(pelicula);
-            return ResponseEntity.status(HttpStatus.CREATED).body(nuevaPelicula);
+            EntityModel<Pelicula> resource = EntityModel.of(nuevaPelicula,
+                    WebMvcLinkBuilder
+                            .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(nuevaPelicula.getId()))
+                            .withSelfRel(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas())
+                            .withRel("all-peliculas"));
+            return ResponseEntity.status(HttpStatus.CREATED).body(resource);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear la pelicula");
         }
     }
 
     @PutMapping("/{id}")
-    public Pelicula updatePelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
-        return peliculaService.updatePelicula(id, pelicula);
+    public ResponseEntity<?> updatePelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+        Pelicula updatedPelicula = peliculaService.updatePelicula(id, pelicula);
+        EntityModel<Pelicula> resource = EntityModel.of(updatedPelicula,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas())
+                        .withRel("all-peliculas"));
+        return ResponseEntity.ok(resource);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePelicula(@PathVariable Long id)
-    {
+    public ResponseEntity<?> deletePelicula(@PathVariable Long id) {
         peliculaService.deletePelicula(id);
+        return ResponseEntity.ok().body(
+                CollectionModel.of(Collections.emptyList(),
+                        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas())
+                                .withRel("all-peliculas")));
     }
 }
